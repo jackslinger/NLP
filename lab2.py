93bb8832a12e329bc00f71bf84427188370111bb
Jack Slinger <jackslinger10@gmail.com>
2016-02-24 21:12:59 +0000

    Added weight averaging and made the output preaty.

--- lab2.py
+++ lab2.py
@@ -4,17 +4,18 @@ import numpy as np
 from random import shuffle
 import os
 import pickle
+import time
 
 #FOLDER_PATH = '/software/Python/NLP/txt_sentoken/'
-#FOLDER_PATH = os.getcwd()
-FOLDER_PATH = '/home/jack/Downloads/txt_sentoken/'
+FOLDER_PATH = os.getcwd() + '/'
+#FOLDER_PATH = '/home/jack/Downloads/txt_sentoken/'
 SAVE_FILE_NAME = 'review_vectors'
 KEYSET_FILE_NAME = 'keyset_vector'
-LOAD_FROM_FILE = True
-SAVE_TO_FILE = True
+LOAD_FROM_FILE = False
+SAVE_TO_FILE = False
 SHUFFLE = True
-ITERATIONS = 5
-AVERAGE_WEIGHTS = False
+ITERATIONS = 8
+AVERAGE_WEIGHTS = True
 
 def create_bag_of_words(file_path):
     file = open(FOLDER_PATH + file_path)
@@ -89,8 +90,9 @@ def perceptron_train(examples, weights, iterations=1, average_weights=False):
             np.random.shuffle(examples)
 
         if AVERAGE_WEIGHTS:
-            weights_sum = weights
+            weights_avg = np.zeros((2,len(examples[0][0])))
 
+        count = 0
         for example in examples:
             vector = example[0]
             label = example[1]
@@ -98,12 +100,24 @@ def perceptron_train(examples, weights, iterations=1, average_weights=False):
             prediction = perceptron_predict(vector, weights)
 
             if bool(prediction) != label:
-                weights[label] += vector
-                weights[prediction] -= vector
+                if prediction == 1:
+                    weights[0] += vector
+                    weights[1] -= vector
+                elif prediction == 0:
+                    weights[1] += vector
+                    weights[0] -= vector
+
+                if AVERAGE_WEIGHTS:
+                    count += 1
+                    # Rolling average using
+                    # avg -= avg / count
+                    # avg += new_sample / count
+                    weights_avg -= weights_avg / count
+                    weights_avg += weights / count
 
     if AVERAGE_WEIGHTS:
-        average = weights_sum / float(iterations * len(examples))
-        return average
+        #average = weights_sum / float(iterations * len(examples))
+        return weights_avg
     else:
         return weights
 
@@ -117,6 +131,62 @@ def perceptron_test(examples, weights):
 
     return predictions
 
+def compute_accuracy(predictions, examples, print_to_screen=False):
+    correct = 0
+    pos = 0
+    neg = 0
+
+    for i in range(0,len(predictions)):
+        prediction = predictions[i]
+        actual = examples[i][1]
+
+        if prediction:
+            pos += 1
+
+        if not prediction:
+            neg += 1
+
+        if prediction and actual:
+            correct += 1
+        elif not prediction and not actual:
+            correct += 1
+
+    accuracy = correct / float(len(predictions))
+
+    if print_to_screen:
+        print 'Correct: ' + str(correct)
+        print 'Positive: ' + str(pos)
+        print 'Negative: ' + str(neg)
+        print 'Accuracy: ' + str(accuracy)
+
+    return accuracy
+
+def print_top_words(weights, key_set, limit):
+    sorted_neg_weights = np.argsort(weights[0])
+    length = len(sorted_neg_weights)
+    print 'Top 10 Negative Words'
+    for i in sorted_neg_weights[length-10:length]:
+        print key_set[i]
+
+    print '-' * 20
+
+    sorted_pos_weights = np.argsort(weights[1])
+    length = len(sorted_pos_weights)
+    print 'Top 10 Positive Words'
+    for i in sorted_pos_weights[length-10:length]:
+        print key_set[i]
+
+
+
+
+print '-' * 20
+print 'Perceptron settings'
+print 'Load from file: ' + str(LOAD_FROM_FILE)
+print 'Save to file: ' + str(SAVE_TO_FILE)
+print 'Shuffling: ' + str(SHUFFLE)
+print 'Averaging weights: ' + str(AVERAGE_WEIGHTS)
+print 'Iterations: ' + str(ITERATIONS)
+print '-' * 20
 
 if LOAD_FROM_FILE:
     try:
@@ -135,51 +205,49 @@ else:
 train = np.concatenate((vectors[0:800], vectors[1000:1800]))
 test = np.concatenate((vectors[800:1000], vectors[1800:2000]))
 
-if SHUFFLE:
-    np.random.shuffle(train)
-    np.random.shuffle(test)
-
 weights = np.zeros((2,len(vectors[0][0])))
+
+print 'Training the perceptron'
 weights = perceptron_train(train, weights, ITERATIONS)
 predictions = perceptron_test(test, weights)
 
-print predictions[0]
+print '-' * 20
+compute_accuracy(predictions, test, True)
 
-correct = 0
-pos = 0
-neg = 0
+print '-' * 20
+print_top_words(weights, key_set, 10)
 
-for i in range(0,len(predictions)):
-    prediction = predictions[i]
-    actual = test[i][1]
+# times = []
+# for iterations in range(10,11):
+#     start = time.time()
+#
+#     weights = np.zeros((2,len(vectors[0][0])))
+#     weights = perceptron_train(train, weights, iterations)
+#     predictions = perceptron_test(test, weights)
+#
+#     compute_accuracy(predictions, test)
+#
+#     end = time.time()
+#
+#     time_taken = end - start
+#     print time_taken
+#     times.append(time_taken)
+#
+# print times
 
-    if prediction:
-        pos += 1
 
-    if not prediction:
-        neg += 1
-
-    if prediction and actual:
-        correct += 1
-    elif not prediction and not actual:
-        correct += 1
-
-print 'Correct: ' + str(correct)
-print 'Positive: ' + str(pos)
-print 'Negative: ' + str(neg)
-
-accuracy = correct / float(len(predictions))
-
-print 'Accuracy: ' + str(accuracy)
-
-sorted_neg_weights = np.argsort(weights[0])
-length = len(sorted_neg_weights)
-print 'Top 10 Negative Words'
-for i in sorted_neg_weights[length-10:length]:
-    print key_set[i]
-
-sorted_pos_weights = np.argsort(weights[1])
-length = len(sorted_pos_weights)
-print 'Top 10 Positive Words'
-for i in sorted_pos_weights[length-10:length]:
-    print key_set[i]
+# iter_results = []
+# for iterations in range(0,1):
+#     results = []
+#     for run in range(0,20):
+#         weights = np.zeros((2,len(vectors[0][0])))
+#         weights = perceptron_train(train, weights, iterations)
+#         predictions = perceptron_test(test, weights)
+#
+#         results.append(compute_accuracy(predictions, test, True))
+#
+#         #print_top_words(weights, key_set, 10)
+#
+#     print results
+#     print np.mean(results)
+#     iter_results.append(results)
